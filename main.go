6351752b3f2e85bfcd5aec6eaebecde716e86158
qbhy <96qbhy@gmail.com>
2020-12-10 18:11:41 +0800

    添加readme

--- main.go
+++ main.go
@@ -51,18 +51,14 @@ func main() {
 	// 开启操作管道协程
 	data.Start()
 
-	//data.Set("a", 1)
-
-	//fmt.Println(data.Incr("a", 100))
-
 	router := gin.Default()
 
-	// 对外调用做一些映射
 
 	router.GET("/", func(context *gin.Context) {
 		context.String(http.StatusOK, "hello world")
 	})
 
+	// 对外调用做一些映射，后面版本会改成 tcp
 	router.POST("/cmd", func(context *gin.Context) {
 		ch := data.Call(context.PostForm("cmd"), context.PostForm("key"), util.StrToInt(context.PostForm("arg")))
 		var result interface{} = nil
